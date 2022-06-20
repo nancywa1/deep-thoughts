d4ffb9d0d30c8f7490fecc38dd98c73d75f7df07
nancywa1 <nancy.wang0717@gmail.com>
2022-06-20 17:30:28 -0600

    Add react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
-import React from "react";
-
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-import Home from "./pages/Home";
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
-  ApolloProvider,
   ApolloClient,
   InMemoryCache,
+  ApolloProvider,
   createHttpLink,
-} from "@apollo/client";
+} from '@apollo/client';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
-  uri: "/graphql",
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
@@ -24,26 +29,42 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route 
+                path="/profile" 
+                element={<Profile />} 
+              />
+              <Route 
+                path="/thought/:id" 
+                element={<SingleThought />} 
+              />
+              <Route 
+                path="*" 
+                element={<NoMatch />} 
+              />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
-// function App() {
-//   return (
-//     <div className='flex-column justify-flex-start min-100-vh'>
-//       <Header />
-//       <div className='container'>
-//         <Home />
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { useQuery } from "@apollo/client";
-import { QUERY_THOUGHTS } from "../utils/queries";
-import ThoughtList from "../components/ThoughtList";
+import React from 'react';
+import ThoughtList from '../components/ThoughtList';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
-  // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
   const thoughts = data?.thoughts || [];
-  console.log(thoughts);
+
   return (
     <main>
       <div className="flex-row justify-space-between">
@@ -25,14 +25,5 @@ const Home = () => {
     </main>
   );
 };
-// const Home = () => {
-//   return (
-//     <main>
-//       <div className='flex-row justify-space-between'>
-//         <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
-//       </div>
-//     </main>
-//   );
-// };
 
 export default Home;
